8c043a389974a7979ac8d20b293f24e363ab428e
tuananh940205 <tuan.anh.94.02.05@gmail.com>
2020-06-04 11:12:20 +0700

    Commit

---
+++ Assets/Scripts/Test/Test08.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Test08 : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            spriteRenderer.DOColor(new Color(0, 0, 0, 0), 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            spriteRenderer.DOFade(0, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+    }
+}
