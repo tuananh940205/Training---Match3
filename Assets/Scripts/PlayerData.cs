0e5da98013b5611d4148994de11cdbfd413489ef
tuananh940205 <tuan.anh.94.02.05@gmail.com>
2020-06-09 16:24:01 +0700

    Commit

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PlayerData
+{
+    public string level = "";
+    public int score = 0;
+    public int counter = 0;
+    public TileController[,] tilesArray;
+}
