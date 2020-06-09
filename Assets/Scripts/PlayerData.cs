94c6d913377f1aba6df825a74f3bf0d9a87f063c
tuananh940205 <tuan.anh.94.02.05@gmail.com>
2020-06-09 18:00:03 +0700

    Commit

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -4,8 +4,27 @@ using System;
 
 public class PlayerData
 {
-    public string level = "";
-    public int score = 0;
-    public int counter = 0;
+    public string level;
+    public int score;
+    public int counter;
     public TileController[,] tilesArray;
+
+    public PlayerData(string level, int score, int counter, TileController[,] tilesArray)
+    {
+        this.level = level;
+        this.score = score;
+        this.counter = counter;
+        this.tilesArray = tilesArray;
+    }
+
+    void SetUpArray(TileController[,] tilesArray)
+    {
+        for (int y = 0; y < tilesArray.GetLength(1); y++)
+        {
+            for (int x = 0; x < tilesArray.GetLength(0); x++)
+            {
+                
+            }
+        }
+    }
 }
